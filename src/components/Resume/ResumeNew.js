5d198d44fbdc08325521c3e4bb72d978b6b76181
RadhakantaChhatria <9999radhakant@gmail.com>
2022-05-29 00:47:15 +0530

    one

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/RADHAKANTA_CHHATRIA_Resume_25-05-2022-20-50-56.pdf";
+import pdf from "../../Assets/../Assets/Radhakant.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/RadhakantaChhatria/portfolio-master/master/src/Assets/RADHAKANTA_CHHATRIA_Resume_25-05-2022-20-50-56.pdf";
+  "https://raw.githubusercontent.com/RadhakantaChhatria/portfolio-master/master/src/Assets/Radhakant.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
