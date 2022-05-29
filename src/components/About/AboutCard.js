5a0282ec5dbc3bf250f288693dd17dd217cc5bd6
RadhakantaChhatria <9999radhakant@gmail.com>
2022-05-29 13:22:53 +0530

    ok

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -28,7 +28,7 @@ function AboutCard() {
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
+          <p style={{ color: "#5d8cec" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
           <footer className="blockquote-footer">Radhakanta</footer>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,7 +18,7 @@ function Projects() {
           My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Here are the few projects I've worked on.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
