7899861583c6515cdf51d2cb4467cde73e31fd61
RadhakantaChhatria <9999radhakant@gmail.com>
2022-05-30 01:00:27 +0530

    add

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/RadhakantaChhatria/portfolio-master/master/src/Assets/Radhakant.pdf";
+  "https://raw.githubusercontent.com/RadhakantaChhatria/RADHAKANT-PORTFOLIO/master/src/Assets/Radhakant.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
